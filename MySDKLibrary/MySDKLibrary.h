c06302575bf7edb60683a4d0305ad9948ea1b381
kangkai <>
2018-07-24 15:23:21 +0800

    新建xcode工程-swift项目

---
+++ MySDKLibrary/MySDKLibrary.h
@@ -0,0 +1,19 @@
+//
+//  MySDKLibrary.h
+//  MySDKLibrary
+//
+//  Created by kangkai on 2018/7/24.
+//  Copyright © 2018年 kangkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MySDKLibrary.
+FOUNDATION_EXPORT double MySDKLibraryVersionNumber;
+
+//! Project version string for MySDKLibrary.
+FOUNDATION_EXPORT const unsigned char MySDKLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySDKLibrary/PublicHeader.h>
+
+
